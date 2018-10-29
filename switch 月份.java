93f6b216c7a5aa2d66a28483b12581f00e7c0498
107021155 <43198527+107021155@users.noreply.github.com>
2018-10-29 23:17:51 +0800

    Add files via upload

---
+++ switch 月份.java
@@ -0,0 +1,74 @@
+import javax.xml.bind.SchemaOutputResolver;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int v1 = scn.nextInt();
+        switch (v1){
+            case 3:
+                System.out.println("March");
+                break;
+            case 4:
+                System.out.println("April");
+                break;
+            case 5:
+                System.out.println("May");
+                break;
+            case 6:
+                System.out.println("June");
+                break;
+            case 7:
+                System.out.println("July");
+                break;
+            case 8:
+                System.out.println("August");
+                break;
+            case 9:
+                System.out.println("September");
+                break;
+            case 10:
+                System.out.println("October");
+                break;
+            case 11:
+                System.out.println("November");
+                break;
+            case 12:
+                System.out.println("December");
+                break;
+            case 1:
+                System.out.println("January");
+                break;
+            case 2:
+                System.out.println("February");
+                break;
+
+        }
+
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      //  String st = scn.next();
+       // int v1 = Integer.parseInt(st,16);
+        //System.out.println((char)v1);
+
