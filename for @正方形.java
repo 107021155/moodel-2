c55fb08c59cfed09abba71dd60cd9b64e879459f
107021155 <43198527+107021155@users.noreply.github.com>
2018-10-31 15:42:46 +0800

    Update for @正方形.java

--- for @正方形.java
+++ for @正方形.java
@@ -6,7 +6,7 @@ public class Main {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j <n; j++) {
+            for (int j = 0; j < n; j++) {
                 System.out.print("@");
             }
             System.out.println();
