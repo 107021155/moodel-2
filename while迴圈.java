12b5cfb1c294ad63be6c39618047452b68434645
107021155 <43198527+107021155@users.noreply.github.com>
2018-11-07 15:12:35 +0800

    Add files via upload

---
+++ while迴圈.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n =0;
+        int sum=0;
+        int count=0;
+        while(n!=-999){
+            sum=sum+n;
+            n=scn.nextInt();
+            count = count+1;
+        }
+        System.out.println(sum);
+        System.out.println(sum/(count-1));
+    }
+}
