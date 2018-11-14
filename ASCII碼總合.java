ece6665f9fcb0131da1a137445d47cdb98e3863c
107021155 <43198527+107021155@users.noreply.github.com>
2018-11-14 15:52:58 +0800

    Add files via upload

--- ASCII碼總合.java
+++ ASCII碼總合.java
@@ -5,9 +5,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         String str = scn.nextLine();
-        int sum=0;
-        for (int i=0;i< str.length();i++){
-            sum=sum+(int)str.charAt(i);
+        int sum=0,n = scn.nextInt();
+        for (int j=0;j<n;j++) {
+            for (int i = 0; i < str.length(); i++) {
+                sum = sum + (int) str.charAt(i);
+            }
         }
         System.out.println(sum);
     }
