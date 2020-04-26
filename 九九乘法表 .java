7b78ab48a9cfb4fc4a11ffba1289afb516cbf433
107021155 <43198527+107021155@users.noreply.github.com>
2020-04-27 05:49:58 +0800

    Update 九九乘法表 .java

--- 九九乘法表 .java
+++ 九九乘法表 .java
@@ -7,7 +7,7 @@ public class Main {
         int n = scn.nextInt();
         for (int i=1;i<=n;i++){
             for (int j=1;j<=n;j++){
-                System.out.print((i*j)+"\f");
+                System.out.print((i*j)+"\t");
             }
             System.out.println();
         }
